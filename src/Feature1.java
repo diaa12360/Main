792561bbff97fbea6563b6210e46598880976dfd
Deya Abu-Elwan <deyacc12@gmail.com>
2023-05-25 15:27:26 +0300

    Add Feature1.java Class.

---
+++ src/Feature1.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class Feature1 {
+    private Date createDate;
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+}
